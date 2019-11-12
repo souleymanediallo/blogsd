ab65f63962aac39f762c0a2556efd668c30c9bdb
souleymane <souleymanedialloo@yahoo.fr>
2019-11-12 10:42:11 +0100

    add model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ('title', 'date_posted', 'update',)
+    list_display_links = ('title',)
+    search_fields = ['title']
+
