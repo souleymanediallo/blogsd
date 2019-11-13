cab5aac7ee1edc57d7bc7bcca03985d102fdda70
souleymane <souleymanedialloo@yahoo.fr>
2019-11-13 11:12:51 +0100

    add CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    # path('', views.index, name="home"),
+    path('', PostListView.as_view(), name="home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
+    path('new/', PostCreateView.as_view(), name="post-create"),
     path('about/', views.about, name="about")
 ]
+
+# <app>/<model>_<viewtype>.html
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(default='profile.png', upload_to='images')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 50 or img.width > 50:
+            img_size = (50, 50)
+            img.thumbnail(img_size)
+            img.save(self.image.path)
+
+
+
